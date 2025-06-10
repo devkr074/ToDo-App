69f6e329be9fc6c55fdd689eff62b867b50a1604
Dev Kumar <devkr074@gmail.com>
2025-06-10 22:53:40 +0530

    Created Task Controller and Service

---
+++ src/main/java/com/app/todo/controller/TaskController.java
@@ -0,0 +1,26 @@
+package com.app.todo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.todo.model.Task;
+import com.app.todo.service.TaskService;
+
+@Controller
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @GetMapping
+    public String getTask(Model model){
+        List<Task> tasks=taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+}
---
+++ src/main/java/com/app/todo/service/TaskService.java
@@ -0,0 +1,21 @@
+package com.app.todo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.app.todo.model.Task;
+import com.app.todo.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+    
+}
