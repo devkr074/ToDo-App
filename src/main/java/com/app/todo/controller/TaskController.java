cefa143cbd0c9112b108fb1f4f2d94afd96de462
Dev Kumar <devkr074@gmail.com>
2025-06-11 09:27:24 +0530

    Updated Task Controller, Task Service and Task Template

--- src/main/java/com/app/todo/controller/TaskController.java
+++ src/main/java/com/app/todo/controller/TaskController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.todo.model.Task;
 import com.app.todo.service.TaskService;
 
 @Controller
-@RequestMapping("/tasks")
 public class TaskController {
     private final TaskService taskService;
     public TaskController(TaskService taskService) {
@@ -22,5 +22,10 @@ public class TaskController {
         List<Task> tasks=taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/";
     }
 }
--- src/main/java/com/app/todo/service/TaskService.java
+++ src/main/java/com/app/todo/service/TaskService.java
@@ -17,5 +17,12 @@ public class TaskService {
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
     
 }
